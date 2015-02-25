7076d7554d97d758bca59b36688e1fa9a4d915c5
Craig Mulligan <craig@firingsuad.co.za>
2015-02-25 17:07:58 +0000

    init

--- server.js
+++ server.js
@@ -15,37 +15,32 @@ app.get("/send", function(request, response){
 
     // Apply command
     if (queryData.pic == 'true') {
-        selfie(); 
-        console.log("lamp: ON");
-    }
+        	var camera = new RaspiCam({
+			mode: "photo",
+			output: "../data/image.jpg",
+			encoding: "jpg",
+			timeout: 100 
+			});
+
+			camera.on("started", function( err, timestamp ){
+				console.log("photo started at " + timestamp );
+			});
+
+
+			camera.on("read", function( err, timestamp, filename ){
+				console.log("photo image captured with filename: " + filename );
+				//we can now do stuff with the captured image, which is stored in /data
+			});
+
+			camera.on("exit", function( timestamp ){
+				console.log("photo child process has exited at " + timestamp );
+			});
+
+			camera.start();
+		        console.log("lamp: ON");
+	}
+
     // Answer
     response.writeHead(200, {"Content-Type": "text/html"});
     response.end();
 });
-
-
-
-function selfie(){
-	var camera = new RaspiCam({
-	mode: "photo",
-	output: "../data/image.jpg",
-	encoding: "jpg",
-	timeout: 100 
-	});
-
-	camera.on("started", function( err, timestamp ){
-		console.log("photo started at " + timestamp );
-	});
-
-
-	camera.on("read", function( err, timestamp, filename ){
-		console.log("photo image captured with filename: " + filename );
-		//we can now do stuff with the captured image, which is stored in /data
-	});
-
-	camera.on("exit", function( timestamp ){
-		console.log("photo child process has exited at " + timestamp );
-	});
-
-	camera.start();
-}
